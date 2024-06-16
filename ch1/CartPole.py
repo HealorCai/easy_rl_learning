d51a87eaf5bd6c63b4ad7042b53d722ec39ff15f
HealorCai <1326302419@qq.com>
2024-06-16 23:42:22 +0800

    add MountainCar

--- ch1/CartPole.py
+++ ch1/CartPole.py
@@ -2,9 +2,15 @@ import gym  # 导入 Gym 的 Python 接口环境包
 env = gym.make('CartPole-v1')  # 构建实验环境
 env.new_step_api = True
 env.reset()  # 重置一个回合
-for i in range(200):
-    print(i)
+for i in range(20):
+    print("The %d times: " % i)
     env.render()  # 显示图形界面
     action = env.action_space.sample() # 从动作空间中随机选取一个动作
-    env.step(action) # 用于提交动作，括号内是具体的动作
+    observation, reward, done, info, _ = env.step(action)
+    print(observation)
 env.close() # 关闭环境
+
+from gym import envs
+env_specs = envs.registry.values()
+envs_ids = [env_spec.id for env_spec in env_specs]
+print(envs_ids)
